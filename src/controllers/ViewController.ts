cff58ddf0f3afedbac338274e09832e1f37e64a2
Jose <menendez@autonomic.ai>
2020-01-17 12:53:22 -0800

    Upgrade to typescript-react-stub

--- src/controllers/ViewController.ts
+++
@@ -1,8 +0,0 @@
-import {View} from "../views/View";
-
-
-export class ViewController{
-
-    constructor(readonly view: View){}
-
-}
--- src/views/View.ts
+++
@@ -1,7 +0,0 @@
-
-
-export class View implements IView{
-    constructor(readonly element: HTMLElement){
-        element.classList.add('this-is-a-view')
-    }
-}
